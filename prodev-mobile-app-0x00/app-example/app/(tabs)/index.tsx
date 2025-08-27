fff3a94ac76ad8faa21e98e4d1d8f08175804b74
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-27 06:51:55 +0100

    Update index.tsx

--- prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -1,1 +1,9 @@
+import { Text, View } from 'react-native';
 
+export default function TabOneScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>First App Created</Text>
+    </View>
+  );
+}
